1c3f3129b638272e29c7cd75705021d21c140be3
chenxuuu <lolicon@papapoi.com>
2020-09-30 15:03:08 +0800

    可以显示图片了

--- USB-Screen/USB-Screen/MainWindow.xaml.cs
+++ USB-Screen/USB-Screen/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,26 +32,34 @@ namespace USB_Screen
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            UInt16 x = 4;
-            UInt16 y = 4;
+            byte x = 240;
+            byte y = 240;
 
             List<byte> data = new List<byte>();
 
             Screen.AddData(data, new List<byte>() { 0x2a }, true);
             Screen.AddData(data, new List<byte>() { 0, 0 });
-            Screen.AddData(data, new List<byte>() { (byte)((x - 1) >> 8), (byte)((x - 1) % 256) });
+            Screen.AddData(data, new List<byte>() { 0, (byte)(x - 1) });
             Screen.AddData(data, new List<byte>() { 0x2b }, true);
             Screen.AddData(data, new List<byte>() { 0, 0 });
-            Screen.AddData(data, new List<byte>() { (byte)((y - 1) >> 8), (byte)((y - 1) % 256) });
+            Screen.AddData(data, new List<byte>() { 0, (byte)(y - 1) });
             Screen.AddData(data, new List<byte>() { 0x2c }, true);
+
+            Bitmap img = new Bitmap(@"1.png");
 
             List<byte> sd = new List<byte>();
             for (int i = 0; i < x; i++)
-            for (int j = 0; j < y; j++)
-            {
-                sd.Add(0xff);
-                sd.Add(0xff);
-            }
+                for (int j = 0; j < y; j++)
+                {
+                    var color = img.GetPixel(y-i-1, j);
+                    //rrrr rggg gggb bbbb
+                    var rgb565 = color.R / 8 * 2048 + color.G / 4 * 32 + color.B / 8;
+                    sd.Add((byte)(rgb565 / 256));
+                    sd.Add((byte)(rgb565 % 256));
+                }
+
+
+            //MessageBox.Show(sd.Count.ToString());
             Screen.AddData(data, sd);
 
             var r = Hid.SendBytes(data.ToArray());
