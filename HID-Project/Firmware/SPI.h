cd799766587b218387f73b45ab162c7bf7fb12f9
Antecer <antecer@outlook.com>
2020-10-10 14:54:06 +0800

    调整硬件SPI上拉电压,解决硬SPI刷屏闪烁问题.

--- HID-Project/Firmware/SPI.h
+++ HID-Project/Firmware/SPI.h
@@ -73,9 +73,9 @@ void SPI_Send_CMD(UINT8 dat)
 void SPIMasterModeSet()
 {
 	SPI0_CTRL	= 0x60;		// 模式0		
-	SPI0_CK_SE	= 0x02;		// 分频系数2，SPI频率=Fsys/2
-    P1_MOD_OC  |= 0xF0;		// 高4位设置位开漏输出
-    P1_DIR_PU  |= 0xF0;		// 高4位启用上拉电阻
+//	SPI0_CK_SE	= 0x02;		// 分频系数2，SPI频率=Fsys/2
+//    P1_MOD_OC  |= 0xF0;		// 高4位设置位开漏输出
+//    P1_DIR_PU  |= 0xF0;		// 高4位启用上拉电阻
 }
 /*******************************************************************************
 * Function Name  : SPI_SendDAT(UINT8 dat)
@@ -109,6 +109,7 @@ void SPI_SendCMD(UINT8 dat)
 *******************************************************************************/
 void LCD_Init(void)
 {
+	SPI0_CK_SE	= 0x02;		// 分频系数2，SPI频率=Fsys/2
 	LCD_BLK = 1;		// 打开背光
 	LCD_CS	= 0;		// LCD片选使能（允许操作屏幕）
 	mDelaymS(20);		// 等待
@@ -118,88 +119,88 @@ void LCD_Init(void)
 	mDelaymS(20);		// 等待
 	LCD_CS = 1;
 	LCD_CS = 0;
-	SPIMasterModeSet();	// 启用硬件SPI
-	SPI_SendCMD(0x11);
-	SPI_SendCMD(0x36);
-	SPI_SendDAT(0x00);
-	SPI_SendCMD(0x3A);
-	SPI_SendDAT(0x05);
-	SPI_SendCMD(0xB2);
-	SPI_SendDAT(0x0C);
-	SPI_SendDAT(0x0C);
-	SPI_SendDAT(0x00);
-	SPI_SendDAT(0x33);
-	SPI_SendDAT(0x33);
-	SPI_SendCMD(0xB7);
-	SPI_SendDAT(0x35);
-	SPI_SendCMD(0xBB);
-	SPI_SendDAT(0x32); //Vcom=1.35V
-	SPI_SendCMD(0xC2);
-	SPI_SendDAT(0x01);
-	SPI_SendCMD(0xC3);
-	SPI_SendDAT(0x15); //GVDD=4.8V  颜色深度
-	SPI_SendCMD(0xC4);
-	SPI_SendDAT(0x20); //VDV, 0x20:0v
-	SPI_SendCMD(0xC6);
-	SPI_SendDAT(0x0F); //0x0F:60Hz
-	SPI_SendCMD(0xD0);
-	SPI_SendDAT(0xA4);
-	SPI_SendDAT(0xA1);
-	SPI_SendCMD(0xE0);
-	SPI_SendDAT(0xD0);
-	SPI_SendDAT(0x08);
-	SPI_SendDAT(0x0E);
-	SPI_SendDAT(0x09);
-	SPI_SendDAT(0x09);
-	SPI_SendDAT(0x05);
-	SPI_SendDAT(0x31);
-	SPI_SendDAT(0x33);
-	SPI_SendDAT(0x48);
-	SPI_SendDAT(0x17);
-	SPI_SendDAT(0x14);
-	SPI_SendDAT(0x15);
-	SPI_SendDAT(0x31);
-	SPI_SendDAT(0x34);
-	SPI_SendCMD(0xE1);
-	SPI_SendDAT(0xD0);
-	SPI_SendDAT(0x08);
-	SPI_SendDAT(0x0E);
-	SPI_SendDAT(0x09);
-	SPI_SendDAT(0x09);
-	SPI_SendDAT(0x15);
-	SPI_SendDAT(0x31);
-	SPI_SendDAT(0x33);
-	SPI_SendDAT(0x48);
-	SPI_SendDAT(0x17);
-	SPI_SendDAT(0x14);
-	SPI_SendDAT(0x15);
-	SPI_SendDAT(0x31);
-	SPI_SendDAT(0x34);
-	SPI_SendCMD(0x21);
-	SPI_SendCMD(0x29);
+//	SPIMasterModeSet();	// 启用硬件SPI (使用硬SPI初始化LCD); 使用软SPI初始化能看到效果(硬SPI太快,看不到)
+	SPI_Send_CMD(0x11);
+	SPI_Send_CMD(0x36);
+	SPI_Send_DAT(0x00);
+	SPI_Send_CMD(0x3A);
+	SPI_Send_DAT(0x05);
+	SPI_Send_CMD(0xB2);
+	SPI_Send_DAT(0x0C);
+	SPI_Send_DAT(0x0C);
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0x33);
+	SPI_Send_DAT(0x33);
+	SPI_Send_CMD(0xB7);
+	SPI_Send_DAT(0x35);
+	SPI_Send_CMD(0xBB);
+	SPI_Send_DAT(0x32); //Vcom=1.35V
+	SPI_Send_CMD(0xC2);
+	SPI_Send_DAT(0x01);
+	SPI_Send_CMD(0xC3);
+	SPI_Send_DAT(0x15); //GVDD=4.8V  颜色深度
+	SPI_Send_CMD(0xC4);
+	SPI_Send_DAT(0x20); //VDV, 0x20:0v
+	SPI_Send_CMD(0xC6);
+	SPI_Send_DAT(0x0F); //0x0F:60Hz
+	SPI_Send_CMD(0xD0);
+	SPI_Send_DAT(0xA4);
+	SPI_Send_DAT(0xA1);
+	SPI_Send_CMD(0xE0);
+	SPI_Send_DAT(0xD0);
+	SPI_Send_DAT(0x08);
+	SPI_Send_DAT(0x0E);
+	SPI_Send_DAT(0x09);
+	SPI_Send_DAT(0x09);
+	SPI_Send_DAT(0x05);
+	SPI_Send_DAT(0x31);
+	SPI_Send_DAT(0x33);
+	SPI_Send_DAT(0x48);
+	SPI_Send_DAT(0x17);
+	SPI_Send_DAT(0x14);
+	SPI_Send_DAT(0x15);
+	SPI_Send_DAT(0x31);
+	SPI_Send_DAT(0x34);
+	SPI_Send_CMD(0xE1);
+	SPI_Send_DAT(0xD0);
+	SPI_Send_DAT(0x08);
+	SPI_Send_DAT(0x0E);
+	SPI_Send_DAT(0x09);
+	SPI_Send_DAT(0x09);
+	SPI_Send_DAT(0x15);
+	SPI_Send_DAT(0x31);
+	SPI_Send_DAT(0x33);
+	SPI_Send_DAT(0x48);
+	SPI_Send_DAT(0x17);
+	SPI_Send_DAT(0x14);
+	SPI_Send_DAT(0x15);
+	SPI_Send_DAT(0x31);
+	SPI_Send_DAT(0x34);
+	SPI_Send_CMD(0x21);
+	SPI_Send_CMD(0x29);
 
+	SPI_Send_CMD(0x2A);// 列地址设置(0-239)
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0xEF);
+	SPI_Send_CMD(0x2B);// 行地址设置(0-239)
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0x00);
+	SPI_Send_DAT(0xEF);
+	SPI_Send_CMD(0x2C);// 写LCD数据存储器
     {
         UINT8 v,h;
-		SPI_SendCMD(0x2A);// 列地址设置(0-239)
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0xEF);
-		SPI_SendCMD(0x2B);// 行地址设置(0-239)
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0x00);
-		SPI_SendDAT(0xEF);
-		SPI_SendCMD(0x2C);// 写LCD数据存储器
         for(v=240; v; --v)
         {
             for(h=240; h; --h)
             {
-				SPI_SendDAT(0x00);
-				SPI_SendDAT(0x00);
+				SPI_Send_DAT(0x00);
+				SPI_Send_DAT(0x00);
             }
         }
     }
-	SPI0_CTRL = 0x02;	// 关闭硬件SPI
+//	SPI0_CTRL = 0x02;	// 关闭硬件SPI
 }
 #endif
