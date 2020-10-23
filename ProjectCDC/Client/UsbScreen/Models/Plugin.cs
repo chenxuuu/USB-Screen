bfa899e6f6db8c4ba2238bd7c76afa8ba8c450d9
Antecer <antecer@outlook.com>
2020-10-23 20:31:31 +0800

    试试彩虹渐变,看起来不错.

--- ProjectCDC/Client/UsbScreen/Models/Plugin.cs
+++ ProjectCDC/Client/UsbScreen/Models/Plugin.cs
@@ -13,134 +13,126 @@ using UsbScreen.Interface;
 
 namespace UsbScreen.Models
 {
-    [PropertyChanged.AddINotifyPropertyChangedInterface]
-    class Plugin
-    {
-        /// <summary>
-        /// 是否已启用了某插件？
-        /// </summary>
-        public bool IsEnable { get; set; } = false;
-        private Timer _t = new Timer();
+	[PropertyChanged.AddINotifyPropertyChangedInterface]
+	class Plugin
+	{
+		/// <summary>
+		/// 是否已启用了某插件？
+		/// </summary>
+		public bool IsEnable { get; set; } = false;
+		private Timer _t = new Timer();
 
-        public Plugin()
-        {
-            _t.AutoReset = false;
-            _t.Elapsed += (sender, ee) =>
-            {
-                if(!IsEnable)
-                    return;
-                _t.Stop();
-                try
-                {
-                    //刷新第一屏
-                    (Bitmap pic, int x, int y, long next) =
-                        (ValueTuple<Bitmap, int, int, long>) _refresh.Invoke(_o, new object[] { });
-                    Task.Run(() =>
-                    {
-                        screen?.Show(pic,x,y);
-                        _t.Interval = next;
-                        _t.Start();
-                    });//异步刷，防止卡
-                }
-                catch (Exception e)
-                {
-                    ErrorLogger(e.ToString());
-                }
-            };
-        }
+		public Plugin()
+		{
+			_t.AutoReset = false;
+			_t.Elapsed += (sender, ee) =>
+			{
+				if (!IsEnable)
+					return;
+				_t.Stop();
+				try
+				{
+					//刷新第一屏
+					(Bitmap pic, int x, int y, long next) =
+						(ValueTuple<Bitmap, int, int, long>)_refresh.Invoke(_o, new object[] { });
+					Task.Run(() =>
+					{
+						screen?.Show(pic, x, y);
+						_t.Interval = next;
+						_t.Start();
+					});//异步刷，防止卡
+				}
+				catch (Exception e)
+				{
+					ErrorLogger(e.ToString());
+				}
+			};
+		}
 
-        /// <summary>
-        /// 待操作的屏幕对象
-        /// </summary>
-        public SerialScreen screen;
+		/// <summary>
+		/// 待操作的屏幕对象
+		/// </summary>
+		public SerialScreen screen;
 
-        private MethodInfo _enable = null;
-        private MethodInfo _refresh = null;
-        private MethodInfo _disable = null;
-        private object _o = null;
+		private MethodInfo _enable = null;
+		private MethodInfo _refresh = null;
+		private MethodInfo _disable = null;
+		private object _o = null;
 
-        public static string[] GetPluginList()
-        {
-            return Directory.GetFiles("plugin", "*.dll");
-        }
+		public static string[] GetPluginList()
+		{
+			return Directory.GetFiles("plugin", "*.dll");
+		}
 
-        public static void ErrorLogger(string err)
-        {
-            File.AppendAllText("error_log.txt", 
-                $"{DateTime.Now:[yyyy-MM-dd HH:mm:ss:ffff]}\r\n{err}\r\n\r\n");
-        }
+		public static void ErrorLogger(string err)
+		{
+			File.AppendAllText("error_log.txt", $"{DateTime.Now:[yyyy-MM-dd HH:mm:ss:ffff]}\r\n{err}\r\n\r\n");
+		}
 
-        public bool EnablePlugin(string path)
-        {
-            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
-            if (!File.Exists(path))
-                return false;
-            bool valid = false;
-            try
-            {
-                Assembly asm = Assembly.LoadFile(path);
-                foreach (Type ta in asm.GetExportedTypes())
-                {
-                    Type t = asm.GetType(ta.FullName);
-                    if (t == null
-                        || !t.IsClass
-                        || !t.IsPublic
-                        || t.GetInterface("IScreen") == null
-                       )
-                        continue;
-                    _o = Activator.CreateInstance(t, null);
-                    if (_o == null)
-                        return false;
-                    _enable = ta.GetMethod("Enable");
-                    _refresh = ta.GetMethod("Refresh");
-                    _disable = ta.GetMethod("Disable");
-                    valid = true;
-                    break;
-                }
-            }
-            catch(Exception e)
-            {
-                ErrorLogger(e.ToString());
-                return false;
-            }
-            if (!valid)//插件无效
-                return false;
-            IsEnable = true;//插件状态改为启用
-            Debug.WriteLine("plugin enabled");
-            try
-            {
-                //刷新第一屏
-                var pic = (Bitmap)_enable.Invoke(_o, new object[] { 240, 240 });
-                Task.Run(() =>
-                {
-                    screen?.Show(pic);
-                });//异步刷，防止卡
-                _t.Interval = 1;
-                _t.Start();
-            }
-            catch(Exception e)
-            {
-                ErrorLogger(e.ToString());
-            }
-            return true;
-        }
+		public bool EnablePlugin(string path)
+		{
+			path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+			if (!File.Exists(path))
+				return false;
+			bool valid = false;
+			try
+			{
+				Assembly ass = Assembly.LoadFile(path);
+				foreach (Type ta in ass.GetExportedTypes())
+				{
+					Type t = ass.GetType(ta.FullName);
+					if (t == null || !t.IsClass || !t.IsPublic || t.GetInterface("IScreen") == null) continue;
+					_o = Activator.CreateInstance(t, null);
+					if (_o == null) return false;
+					_enable = ta.GetMethod("Enable");
+					_refresh = ta.GetMethod("Refresh");
+					_disable = ta.GetMethod("Disable");
+					valid = true;
+					break;
+				}
+			}
+			catch (Exception e)
+			{
+				ErrorLogger(e.ToString());
+				return false;
+			}
+			if (!valid) return false;//插件无效
+			IsEnable = true;//插件状态改为启用
+			Debug.WriteLine($"[目标插件已启用] ${path} is running...");
+			try
+			{
+				//刷新第一屏
+				var pic = (Bitmap)_enable.Invoke(_o, new object[] { 240, 240 });
+				Task.Run(() =>
+				{
+					screen?.Show(pic);
+				});//异步刷，防止卡
+				_t.Interval = 1;
+				_t.Start();
+			}
+			catch (Exception e)
+			{
+				ErrorLogger(e.ToString());
+			}
+			return true;
+		}
 
-        public void Disable()
-        {
-            IsEnable = false;
-            _t.Stop();
-            if(_o != null && _disable != null)
-            {
-                try
-                {
-                    _disable.Invoke(_o, null);
-                    Debug.WriteLine("plugin disabled");
-                }
-                catch(Exception e)
-                {
-                    ErrorLogger(e.ToString());
-                }
-            }
-        }
-    }
+		public void Disable()
+		{
+			IsEnable = false;
+			_t.Stop();
+			if (_o != null && _disable != null)
+			{
+				try
+				{
+					_disable.Invoke(_o, null);
+					Debug.WriteLine("plugin disabled");
+				}
+				catch (Exception e)
+				{
+					ErrorLogger(e.ToString());
+				}
+			}
+		}
+	}
 }
--- ProjectCDC/Client/plugins/Clock/Clock/Class1.cs
+++ ProjectCDC/Client/plugins/Clock/Clock/Class1.cs
@@ -1,49 +1,64 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clock
 {
-    public class Clock : IScreen
-    {
-        private Bitmap _pic;
-        private Graphics _g;
-        private Bitmap _pclock;
-        private Graphics _gclock;
-        private int _width;
-        private int _height;
+	public class Clock : IScreen
+	{
+		private Bitmap _pic;
+		private Graphics _g;
+		private Bitmap _pclock;
+		private Graphics _gclock;
+		private int _width;
+		private int _height;
 
-        public Bitmap Enable(int width, int height)
-        {
-            _width = width;
-            _height = height;
-            _pic = new Bitmap(width, height);
-            _g = Graphics.FromImage(_pic);
-            _g.FillRectangle(Brushes.Black, 0, 0, _width, _height);
-            _pclock = new Bitmap(240, 50);
-            _gclock = Graphics.FromImage(_pclock);
-            return _pic;
-        }
+		public Bitmap Enable(int width, int height)
+		{
+			_width = width;
+			_height = height;
+			_pic = new Bitmap(width, height);
+			_g = Graphics.FromImage(_pic);
+			_g.FillRectangle(Brushes.Black, 0, 0, _width, _height);
+			_pclock = new Bitmap(240, 50);
+			_gclock = Graphics.FromImage(_pclock);
+			return _pic;
+		}
 
-        public void Disable()
-        {
-            _g.Dispose();
-            _pic.Dispose();
-            _gclock.Dispose();
-            _pclock.Dispose();
-        }
+		public void Disable()
+		{
+			_g.Dispose();
+			_pic.Dispose();
+			_gclock.Dispose();
+			_pclock.Dispose();
+		}
 
-        Font font = new Font(new FontFamily("Consolas"), 50f, FontStyle.Bold, GraphicsUnit.Pixel);
-        Random r = new Random();
-        public (Bitmap pic, int x, int y, long next) Refresh()
-        {
-            _gclock.FillRectangle(Brushes.Black, 0, 0, _pclock.Width, _pclock.Height);
-            _gclock.DrawString($"{DateTime.Now:HH:mm:ss}", font, 
-                new SolidBrush(Color.FromArgb(255, r.Next(50, 255), r.Next(50, 255), r.Next(50, 255))), 0, 0);
-            return (_pclock, 0, _height / 2 - 32, 1000);
-        }
-    }
+		Font font = new Font(new FontFamily("Consolas"), 50f, FontStyle.Bold, GraphicsUnit.Pixel);
+		Color colorA = Color.FromArgb(0xF8, 0, 0), colorB = Color.FromArgb(0, 0xFC, 0);
+		private Color GetNextColor(ref Color c)
+		{
+			byte R = c.R, G = c.G, B = c.B, offset = 4;
+			if (R == 0xF8 && G < 0xFC && B == 0x00) G += offset;
+			else if (R > 0x00 && G == 0xFC && B == 0x00) R -= offset;
+			else if (R == 0x00 && G == 0xFC && B < 0xF8) B += offset;
+			else if (R == 0x00 && G > 0x00 && B == 0xF8) G -= offset;
+			else if (R < 0xF8 && G == 0x00 && B == 0xF8) R += offset;
+			else if (R == 0xF8 && G == 0x00 && B > 0x00) B -= offset;
+			c = Color.FromArgb(R, G, B);
+			return c;
+		}
+
+		public (Bitmap pic, int x, int y, long next) Refresh()
+		{
+			_gclock.FillRectangle(Brushes.Black, 0, 0, _pclock.Width, _pclock.Height);
+			_gclock.DrawString($"{DateTime.Now:HH:mm:ss}", font,
+			new LinearGradientBrush(new Point(0, 0), new Point(240, 0), GetNextColor(ref colorA), GetNextColor(ref colorB))
+		, 0, 0);
+			return (_pclock, 0, _height / 2 - 32, 200);
+		}
+	}
 }
