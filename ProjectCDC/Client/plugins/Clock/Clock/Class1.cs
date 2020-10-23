c686e460bd52b592bd08fc24b16ad0bd6eb453f4
chenxuuu <lolicon@papapoi.com>
2020-10-23 18:01:16 +0800

    实现例子插件功能

--- ProjectCDC/Client/plugins/Clock/Clock/Class1.cs
+++ ProjectCDC/Client/plugins/Clock/Clock/Class1.cs
@@ -9,8 +9,10 @@ namespace Clock
 {
     public class Clock : IScreen
     {
-        private Bitmap _pic = null;
+        private Bitmap _pic;
         private Graphics _g;
+        private Bitmap _pclock;
+        private Graphics _gclock;
         private int _width;
         private int _height;
 
@@ -20,7 +22,9 @@ namespace Clock
             _height = height;
             _pic = new Bitmap(width, height);
             _g = Graphics.FromImage(_pic);
-            _g.FillRectangle(Brushes.Gray, 0, 0, _width, _height);
+            _g.FillRectangle(Brushes.Black, 0, 0, _width, _height);
+            _pclock = new Bitmap(240, 50);
+            _gclock = Graphics.FromImage(_pclock);
             return _pic;
         }
 
@@ -28,15 +32,18 @@ namespace Clock
         {
             _g.Dispose();
             _pic.Dispose();
+            _gclock.Dispose();
+            _pclock.Dispose();
         }
 
+        Font font = new Font(new FontFamily("Consolas"), 50f, FontStyle.Bold, GraphicsUnit.Pixel);
         Random r = new Random();
         public (Bitmap pic, int x, int y, long next) Refresh()
         {
-            _g.FillRectangle(
-                new SolidBrush(Color.FromArgb(255, r.Next(0, 255), r.Next(0, 255), r.Next(0, 255))),
-                0, 0, _width, _height);
-            return (_pic, 0, 0, 1000);
+            _gclock.FillRectangle(Brushes.Black, 0, 0, _pclock.Width, _pclock.Height);
+            _gclock.DrawString($"{DateTime.Now:HH:mm:ss}", font, 
+                new SolidBrush(Color.FromArgb(255, r.Next(50, 255), r.Next(50, 255), r.Next(50, 255))), 0, 0);
+            return (_pclock, 0, _height / 2 - 32, 1000);
         }
     }
 }
